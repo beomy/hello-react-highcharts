1541c7a1c9943d80970b5cd1b38129403e32aefd
hyobum <hyob1203@naver.com>
2017-05-21 00:31:38 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# hello-react-highcharts
+
+React.JS에서 hightcharts를 사용하는 예제입니다.
+
+react-highcharts와 highcharts-drilldown를 사용하였습니다.
+
+[이 곳](http://beomy.tistory.com/37)에서 코드 설명을 확인 할 수 있습니다.
---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import HighCharts from './HighCharts/HighCharts';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <HighCharts />
+      </div>
+    );
+  }
+}
+
+export default App;
